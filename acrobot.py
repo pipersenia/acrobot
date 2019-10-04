5c2b5231c4495737414306f3193f6ca90264176c
Aditya Pipersenia <aditya.pipersenia@mailme.com>
2019-10-04 13:55:13 -0400

    initialize flask app

--- acrobot.py
+++ acrobot.py
@@ -1,2 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
 if __name__ == '__main__':
-    print("Hello World!")
+    app.run()
