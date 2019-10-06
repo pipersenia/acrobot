a4dc75c44c378d9d16c2a73d82d73f042a076f1a
Aditya Pipersenia <aditya.pipersenia@mailme.com>
2019-10-06 12:59:05 -0400

    jsonify

--- acrobot.py
+++ acrobot.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 
@@ -9,15 +10,17 @@ def hello():
 
 @app.route('/acrobot', methods=['POST'])
 def slash():
-    return [
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "Wonder what *<>* means?"
+    return jsonify({
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "A message *with some bold text* and _some italicized text_."
+                }
             }
-        }
-    ]
+        ]
+    })
 
 
 if __name__ == '__main__':
